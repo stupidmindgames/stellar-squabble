2384344a29a957ceb11ed5376cade807e0da0938
Stupid Mind Games <github@stupidmindgames.com>
2019-02-22 19:15:02 +0000

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Container, Row, Col} from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <Row>
+          <Col className="unit" xs="12" md="6" lg="3">Status</Col>
+          <Col className="unit" xs="12" md="6" lg="3">Sector</Col>
+          <Col className="unit" xs="12" md="12" lg="6">Galaxy</Col>
+        </Row>
+        <Row>
+          <Col className="unit unit-double" xs="12" md="12" lg="6">Messages</Col>
+          <Col className="unit unit-double" xs="12" md="12" lg="6">
+            <Row>
+              <Col className="unit" xs="12" lg="6">Viewer</Col>
+              <Col className="unit" xs="12" lg="6">Commands</Col>
+            </Row>
+
+            <Row>
+              <Col className="unit" xs="12" lg="6">Systems</Col>
+              <Col className="unit" xs="12" lg="6">Badge</Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
