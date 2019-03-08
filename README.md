89fd542c4483fc0c628924aa1c6ca9f4e0c31ade
Stupid Mind Games <github@stupidmindgames.com>
2019-03-08 19:02:44 +0000

    Add command panel button

--- README.md
+++ README.md
@@ -6,7 +6,6 @@ For more information about this seminal DOS game of yesteryear, see https://arch
 
 ## TODO ##
 
- * Lasers
  * Commands
  * Sector
  * Galaxy
--- src/panels/Commands.js
+++ src/panels/Commands.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import {Col} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 class Commands extends Component { 
+  constructor(props, context) {
+    super(props, context);
+
+    this.handleCommandPanelClick = this.handleCommandPanelClick.bind(this);
+  }
+
+  handleCommandPanelClick() {
+    alert('Command panel');
+  }
+
   render() {
     return (
         <Col className="unit unit--commands" xs="12" lg="6">
@@ -13,7 +24,7 @@ class Commands extends Component {
                         <td>Eff.</td>
                         <td>100%</td>
                     </tr>
-                    
+
                     <tr>
                         <td>Temp.</td>
                         <td>0</td>
@@ -23,6 +34,10 @@ class Commands extends Component {
 
             <div className="unit__inner unit__inner--short unit__inner--commands">
                 <h2>Commands</h2>
+
+                 <Button variant="light" onClick={this.handleCommandPanelClick}>
+                    Command Panel
+                </Button>
             </div>
         </Col>
     );
