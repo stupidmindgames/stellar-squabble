98e8134a919826ca792943620d560ed3b1f9073d
Stupid Mind Games <github@stupidmindgames.com>
2019-03-08 18:55:08 +0000

    Lasers and yet more styling fixes

--- src/panels/Commands.js
+++ src/panels/Commands.js
@@ -5,11 +5,23 @@ class Commands extends Component {
   render() {
     return (
         <Col className="unit unit--commands" xs="12" lg="6">
-            <div className="unit__inner unit__inner--short">
+            <div className="unit__inner unit__inner--short unit__inner--lasers">
                 <h2>Lasers</h2>
+
+                <table>
+                    <tr>
+                        <td>Eff.</td>
+                        <td>100%</td>
+                    </tr>
+                    
+                    <tr>
+                        <td>Temp.</td>
+                        <td>0</td>
+                    </tr>
+                </table>
             </div>
 
-            <div className="unit__inner unit__inner--short">
+            <div className="unit__inner unit__inner--short unit__inner--commands">
                 <h2>Commands</h2>
             </div>
         </Col>
