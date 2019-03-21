fa95f23f6f054505706957e67803a02b4ba63df3
Stupid Mind Games <github@stupidmindgames.com>
2019-03-21 17:49:43 +0000

    Add sector panel

--- src/panels/Status.js
+++ src/panels/Status.js
@@ -6,7 +6,35 @@ class Status extends Component {
     return (
         <Col className="unit unit--status" xs="12" md="6"lg="3">
             <div className="unit__inner">
-                <h2>Status</h2>
+                <div className="div__statusPanel div__statusPanel--date">
+                    <span>Date:</span>
+
+                    <span>3500.0</span>
+                </div>
+
+                <div className="div__statusPanel div__statusPanel--alert div__statusPanel--red">
+                    <span>&raquo;&raquo; Alert &laquo;&laquo;</span>
+                </div>
+
+                <div className="div__gauge div__gauge--energy">
+                    100%
+                </div>
+
+                <div className="div__gauge div__gauge--shields">
+                    100%
+                </div>
+
+                <div className="div__statusPanel div__statusPanel--torps">
+                    9
+                </div>
+
+                <div className="div__statusPanel div__statusPanel--warp">
+                    <span>Warp: 1.0</span>
+                </div>
+
+                <div className="div__statusPanel div__statusPanel--mongols">
+                    <span>Mongols: 23</span>
+                </div>
             </div>
         </Col>
     );
@@ -14,3 +42,4 @@ class Status extends Component {
 }
 
 export default Status;
+
